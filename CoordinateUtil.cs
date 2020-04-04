133a36facebf5f0a8a0303e21deb844550b864d5
Thiago <ThiagoDAraujoS@Gmail.com>
2020-04-04 10:34:46 -0700

    Added base files

---
+++ CoordinateUtil.cs
@@ -0,0 +1,151 @@
+﻿using UnityEngine;
+
+//Coodinate object
+[System.Serializable]
+public struct Coordinate
+{
+    public int x;
+    public int y;
+
+    public static readonly Coordinate north = new Coordinate(0, 1);
+    public static readonly Coordinate northeast = new Coordinate( 1, 1);                    
+    public static readonly Coordinate east = new Coordinate(1, 0);
+    public static readonly Coordinate southeast = new Coordinate( 1,-1);                                   
+    public static readonly Coordinate south = new Coordinate(0, -1);
+    public static readonly Coordinate southwest = new Coordinate(-1,-1);                                  
+    public static readonly Coordinate west = new Coordinate(-1, 0);
+    public static readonly Coordinate northwest = new Coordinate(-1, 1);
+
+    public static readonly Coordinate up = new Coordinate(0, 1);
+    public static readonly Coordinate right = new Coordinate(1, 0);
+    public static readonly Coordinate down = new Coordinate(0, -1);
+    public static readonly Coordinate left = new Coordinate(-1, 0);
+
+    public static readonly Coordinate zero = new Coordinate(0, 0);
+    public static readonly Coordinate one = new Coordinate(1, 1);
+
+    public static Coordinate operator *(Coordinate c1, int c2)
+    {
+        return new Coordinate(c1.x * c2, c1.y * c2);
+    }
+    public static Coordinate operator /(Coordinate c1, int c2)
+    {
+        return new Coordinate(c1.x / c2, c1.y / c2);
+    }
+    public static Coordinate operator +(Coordinate c1, Coordinate c2)
+    {
+        return new Coordinate(c1.x + c2.x, c1.y + c2.y);
+    }
+    public static Coordinate operator -(Coordinate c1, Coordinate c2)
+    {
+        return new Coordinate(c1.x - c2.x, c1.y - c2.y);
+    }
+    public static Coordinate operator +(Coordinate c1, Direction c2)
+    {
+        return c1 + c2.toCoord();
+    }
+    public static Coordinate operator -(Coordinate c1, Direction c2)
+    {
+        return c1 - c2.toCoord();
+    }
+
+    public Coordinate(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public override bool Equals(object obj)
+    {
+        bool result = false;
+
+        if (obj.GetType() == typeof(Coordinate))
+        {
+            Coordinate newObj = (Coordinate)obj;
+            result = (x == newObj.x && y == newObj.y);
+        }
+        return result;
+    }
+
+    public static bool operator == (Coordinate c1, Coordinate c2)
+    {
+        bool result = false;
+        if (c1.x == c2.x && c1.y == c2.y)
+            result = true;
+        return result;
+    }
+
+    public static bool operator != (Coordinate c1, Coordinate c2)
+    {
+        bool result = true;
+        if (c1.x == c2.x && c1.y == c2.y)
+            result = false;
+        return result;
+    }
+
+    public int Distance(Coordinate target){
+        return Mathf.Abs(x - target.x) + Mathf.Abs(y - target.y);
+    }
+
+    public static implicit operator Direction(Coordinate c)
+    {
+        Direction result;
+
+        if (c == north)
+            result = Direction.north;
+
+        else if (c == northeast)
+            result = Direction.northeast;
+
+        else if (c == east)
+            result = Direction.east;
+
+        else if (c == southeast)
+            result = Direction.southeast;
+
+        else if (c == south)
+            result = Direction.south;
+
+        else if (c == southwest)
+            result = Direction.southwest;
+
+        else if (c == west)
+            result = Direction.west;
+
+        else if (c == northwest)
+            result = Direction.northwest;
+
+        else
+            result = Direction.nothing;
+
+        return result;
+    } 
+
+    public Vector3 ToVector3(float value = 0.0f){
+        return new Vector3(x, value, y);
+    }
+    
+    public static implicit operator Vector3(Coordinate c)
+    {
+        return new Vector3(c.x, 0.0f, c.y);
+    }
+
+    public override string ToString(){
+        return "[" + x + "/" + y + "]";
+    }
+  
+    public static implicit operator Coordinate(Vector3 vector)
+    {
+        return new Coordinate(Mathf.RoundToInt(vector.x), Mathf.RoundToInt(vector.z));
+    }
+
+    public static implicit operator Coordinate(Vector2 vector)
+    {
+        return new Coordinate(Mathf.RoundToInt(vector.x), Mathf.RoundToInt(vector.y));
+    }
+    
+    public static Coordinate FromVector3(Vector3 vector)
+    {
+        return new Coordinate(Mathf.RoundToInt(vector.x), Mathf.RoundToInt(vector.z));
+    }
+}
---
+++ DirectionUtil.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using UnityEngine;
+/// <summary>
+/// The four direction enumeration
+/// </summary>
+public enum Direction
+{
+    north,
+    east,
+    south,
+    west,
+    nothing,
+    northeast,
+    southeast,
+    southwest,
+    northwest,
+}
+
+public static class DirectionExtentions
+{
+    public static Direction Perpendicular(this Direction dir)
+    {
+        switch (dir) {
+            case Direction.west:
+                dir = Direction.north;
+                break;
+            case Direction.nothing:
+                dir = Direction.nothing;
+                break;
+            case Direction.northwest:
+                dir = Direction.northeast;
+                break;
+            default:
+                dir = (Direction)(((int)dir) + 1);
+                break;
+        }
+        return dir;
+    } 
+
+    public static Vector3 angle(this Direction dir) {
+        return new Vector3(0.0f, (int)dir * 90.0f - 90.0f, 0.0f);
+    }
+    public static float ToAngle45(this Direction dir)
+    {
+        float result = 0.0f; 
+        switch (dir)
+        {
+            case Direction.north:
+                result = 0.0f;
+                break;
+            case Direction.northeast:
+                result = 45.0f;
+                break;
+            case Direction.east:
+                result = 90.0f;
+                break;
+            case Direction.southeast:
+                result = 135.0f;
+                break;
+            case Direction.south:
+                result = 180.0f;
+                break;
+            case Direction.southwest:
+                result = 225.0f;
+                break;
+            case Direction.west:
+                result = 270.0f;
+                break;
+            case Direction.northwest:
+                result = 315.0f;
+                break;
+        }
+        return result;
+    }
+    public static Coordinate toCoord(this Direction dir)
+    {
+        Coordinate result;
+        switch (dir)
+        {
+            case Direction.north:
+                result = Coordinate.north;
+                break;
+            case Direction.west:
+                result = Coordinate.west;
+                break;
+            case Direction.south:
+                result = Coordinate.south;
+                break;
+            case Direction.east:
+                result = Coordinate.east;
+                break;
+            default:
+                result = Coordinate.zero;
+                break;
+            case Direction.northeast:
+                result = Coordinate.northeast;
+                break;
+            case Direction.southeast:
+                result = Coordinate.southeast;
+                break;
+            case Direction.southwest:
+                result = Coordinate.southwest;
+                break;
+            case Direction.northwest:
+                result = Coordinate.northwest;
+                break;
+        }
+        return result;
+    }
+
+    public static float? toAngle(this Direction dir)
+    {
+        float? result = null;
+        if(dir != Direction.nothing)
+            result = 90 * (float)dir;
+        return result;
+    }
+
+    public static int GetQuadrant(this Direction dir)
+    {
+        int result;
+        switch (dir) {
+            case Direction.north:
+                result = 0;
+                break;
+            case Direction.east:
+                result = 1;
+                break;
+            case Direction.south:
+                result = 2;
+                break;
+            case Direction.west:
+                result = 3;
+                break;
+            default:
+                result = 0;
+                break;
+        }
+        return result;
+    }
+
+    public static Direction QuadrantRotate(this Direction dir, int quadrants)
+    {
+        for (int i = 0; i < quadrants; i++) {
+            switch (dir) {
+                case Direction.north:
+                    dir = Direction.east;
+                    break;
+                case Direction.east:
+                    dir = Direction.south;
+                    break;
+                case Direction.south:
+                    dir = Direction.west;
+                    break;
+                case Direction.west:
+                    dir = Direction.north;
+                    break;
+                case Direction.northeast:
+                    dir = Direction.southeast;
+                    break;
+                case Direction.southeast:
+                    dir = Direction.southwest;
+                    break;
+                case Direction.southwest:
+                    dir = Direction.northwest;
+                    break;
+                case Direction.northwest:
+                    dir = Direction.northeast;
+                    break;
+                default:
+                    dir = Direction.nothing;
+                    break;
+            }
+        }
+        return dir;
+    }
+
+    public static Direction toDirection(this string type)
+    {
+        return (Direction)Enum.Parse(typeof(Direction), type);
+    }
+
+}
